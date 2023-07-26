619e3c5b0f0efe61b1419e4d609cd9b78745d832
Sushan Paudyal <mailtosushan@gmail.com>
2023-07-26 16:57:52 +0545

    Initializing the live editor / page builder

--- resources/js/Pages/LiveEditor.vue
+++ resources/js/Pages/LiveEditor.vue
@@ -1,12 +1,29 @@
 <template>
-    <div class="editor">
-        <span v-html="content"></span>
+    <div id="editor">
     </div>
 </template>
 
 
 <script>
+import 'grapesjs/dist/css/grapes.min.css';
+import grapesjs from "grapesjs";
+
 export default {
-    props: {content: String}
+    props: {content: String},
+    methods: {
+        initializeGrapeJsEditor(){
+          const editor =  grapesjs.init({
+                container: '#editor',
+                components: this.content,
+                storageManager: {
+                    autoload: false,
+                    storeHtml: 1,
+                }
+            })
+        }
+    },
+    mounted(){
+        this.initializeGrapeJsEditor();
+    }
 }
 </script>
